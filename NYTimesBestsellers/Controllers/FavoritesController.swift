7743b45f8eff4c4ce5ab11693a02c70ad0bbfed1
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-02-07 15:24:04 -0500

    added label

--- NYTimesBestsellers/Controllers/FavoritesController.swift
+++ NYTimesBestsellers/Controllers/FavoritesController.swift
@@ -9,22 +9,18 @@
 import UIKit
 
 class FavoritesController: UIViewController {
+    
+    var favoritesView = FavoritesView()
+    
+    override func loadView() {
+        view = favoritesView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- NYTimesBestsellers/Views/FavoritesView.swift
+++ NYTimesBestsellers/Views/FavoritesView.swift
@@ -9,13 +9,40 @@
 import UIKit
 
 class FavoritesView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    public lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello User"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder:coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        configureTitleLabel()
+    }
+    
+    func configureTitleLabel() {
+    addSubview(titleLabel)
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 8)
+        ])
+        
+    }
+    
 }
+
+
+
