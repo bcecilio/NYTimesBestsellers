e6f4e4d932f850b7c4e41fadfc52684012460f7f
Ahad Islam <ahad.islam624@gmail.com>
2020-02-07 15:21:51 -0500

    repudiating nyt-tab controller

--- NYTimesBestsellers/Controllers/NYTTabController.swift
+++ NYTimesBestsellers/Controllers/NYTTabController.swift
@@ -9,22 +9,32 @@
 import UIKit
 
 class NYTTabController: UITabBarController {
+    
+    private lazy var viewController: ViewController = {
+        let vc = ViewController()
+        vc.tabBarItem = UITabBarItem(title: "View", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        return vc
+    }()
+    
+    private lazy var favoritesController: FavoritesController = {
+        let vc = FavoritesController()
+        vc.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "book"), tag: 1)
+        return vc
+    }()
+    
+    private lazy var settingsController: SettingsController = {
+        let vc = SettingsController()
+        vc.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "tray"), tag: 2)
+        return vc
+    }()
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        viewControllers = [viewController, favoritesController, settingsController]
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
