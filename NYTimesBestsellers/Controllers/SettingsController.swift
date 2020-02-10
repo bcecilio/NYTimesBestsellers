5bcfa25fe283bd486c0e0717888e55b363e064e0
Ian Bailey <iankbailey2@gmail.com>
2020-02-10 17:35:39 -0500

    added print to settings controller

--- NYTimesBestsellers/Controllers/SettingsController.swift
+++ NYTimesBestsellers/Controllers/SettingsController.swift
@@ -12,7 +12,7 @@ class SettingsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Hello")
         // Do any additional setup after loading the view.
     }
     
