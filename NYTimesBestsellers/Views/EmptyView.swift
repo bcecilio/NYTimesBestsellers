476322ca6b9b383c22933ce2c44efd4f132b3d78
brendon cecilio <brendon.cecilio@gmail.com>
2020-02-13 17:11:38 -0500

    created emptyView

---
+++ NYTimesBestsellers/Views/EmptyView.swift
@@ -0,0 +1,21 @@
+//
+//  EmptyView.swift
+//  NYTimesBestsellers
+//
+//  Created by Brendon Cecilio on 2/13/20.
+//  Copyright © 2020 Brendon Cecilio. All rights reserved.
+//
+
+import UIKit
+
+class EmptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
