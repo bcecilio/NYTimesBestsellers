9654f729b13faab4aaee7d93c671bcc1d4e8c248
brendon cecilio <brendon.cecilio@gmail.com>
2020-02-13 15:33:55 -0500

    number of lines set to 0 for description label

--- NYTimesBestsellers/Cells/BestsellerCell.swift
+++ NYTimesBestsellers/Cells/BestsellerCell.swift
@@ -29,6 +29,7 @@ class BestsellerCell: UICollectionViewCell {
        let label = UILabel()
         label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         label.font = UIFont(name: "HelveticaNeue-Light", size: 13)
+        label.numberOfLines = 0
         return label
     }()
     
