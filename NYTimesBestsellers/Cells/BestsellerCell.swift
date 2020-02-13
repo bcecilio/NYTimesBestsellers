750e6b35f475d55b1f74169f298c62f1351424a0
brendon cecilio <brendon.cecilio@gmail.com>
2020-02-13 14:45:57 -0500

    created variables for cell elements

--- NYTimesBestsellers/Cells/BestsellerCell.swift
+++ NYTimesBestsellers/Cells/BestsellerCell.swift
@@ -10,4 +10,39 @@ import UIKit
 
 class BestsellerCell: UICollectionViewCell {
     
+    public var bookImageView: UIImageView = {
+       let image = UIImageView()
+        image.image = UIImage(systemName: "book")
+        return image
+    }()
+    
+    public var titleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Length book was on Best Sellers List"
+        return label
+    }()
+    
+    public var bookDescriptionLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Length book was on Best Sellers List"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupImageView()
+    }
+    
+    private func setupImageView() {
+        
+    }
 }
