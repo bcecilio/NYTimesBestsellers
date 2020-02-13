c7086d6a0c5c771b63450cd0c819f71e7216005a
Ahad Islam <ahad.islam624@gmail.com>
2020-02-13 12:11:42 -0500

    BookDetail Model works as well, model is complete with unit testing to check it works.

--- NYTimesBestsellersTests/NYTimesBestsellersTests.swift
+++ NYTimesBestsellersTests/NYTimesBestsellersTests.swift
@@ -32,7 +32,8 @@ class NYTimesBestsellersTests: XCTestCase {
     }
     
     func testBestSellersByList() {
-        let endpoint = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=\(NYTKey.key)"
+        let listNameEncoded = "hardcover-fiction"
+        let endpoint = "https://api.nytimes.com/svc/books/v3/lists/current/\(listNameEncoded).json?api-key=\(NYTKey.key)"
         var books = [Book]()
         let exp = XCTestExpectation(description: "Created test for something")
         
@@ -48,5 +49,28 @@ class NYTimesBestsellersTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 2)
+    }
+    
+    func testBestSellersByBook() {
+        let primaryIsbn13 = "9781524763138"
+        let endpoint = "https://api.nytimes.com/svc/books/v3/reviews.json?isbn=\(primaryIsbn13)&api-key=\(NYTKey.key)"
+        
+        var bookDetails = [BookDetail]()
+        
+        let exp = XCTestExpectation(description: "created test for something")
+        
+        GenericCoderAPI.manager.getJSON(objectType: BookDetailWrapper.self, with: endpoint) { result in
+            switch result {
+            case .failure(let error):
+                XCTFail("Failed to get json: \(error)")
+            case .success(let wrapper):
+                bookDetails = wrapper.results
+                XCTAssertEqual(bookDetails.count, wrapper.numResults)
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 2)
+        
     }
 }
