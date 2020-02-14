7a63f7e05c30cef9bcaf881635fb6bb0c4a394e4
Ahad Islam <ahad.islam624@gmail.com>
2020-02-14 12:33:58 -0500

    Added dependency injection, made sure default view is detailview

--- NYTimesBestsellers/Controllers/BookDetailController.swift
+++ NYTimesBestsellers/Controllers/BookDetailController.swift
@@ -7,8 +7,29 @@
 //
 
 import UIKit
+import DataPersistence
+import ImageKit
 
 class BookDetailController: UIViewController {
+    
+    private let detailVC = BookDetailView()
+    
+    private let dataPersistence: DataPersistence<Book>
+    private let book: Book
+    
+    init(_ dataPersistence: DataPersistence<Book>, book: Book) {
+        self.dataPersistence = dataPersistence
+        self.book = book
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = detailVC
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +37,4 @@ class BookDetailController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
